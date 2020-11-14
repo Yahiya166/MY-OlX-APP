a63ed3c79dcbbf77878341f1ccb5a65a4482bda4
Yahiya166 <muhammadyahiya166@gmail.com>
2020-11-14 15:56:29 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return(
-      <div>
-        <h1 style={{textAlign: 'center', marginTop: '300px'}}> Hello World </h1>
+      <div style={{textAlign: 'center', marginTop: '300px'}}>
+        <h1> Hello World </h1>
+        <p> My Name Is Yahiya </p>
       </div>
     )
   }
